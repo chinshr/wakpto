b859f9b139d93aeab324decd052854622aacd881
David Robbins <dave@wakanda.org>
2012-08-17 08:16:13 -0700

    added date to email log

--- PTOb201/WebFolder/scripts/index.js
+++ PTOb201/WebFolder/scripts/index.js
@@ -31,6 +31,59 @@ if(dd<10){dd='0'+dd}
 if(mm<10){mm='0'+mm}
 var myCurrentDate = mm+'/'+dd+'/'+yyyy;
 
+var dates = {
+    convert:function(d) {
+        // Converts the date in d to a date-object. The input can be:
+        //   a date object: returned without modification
+        //  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+        //   a number     : Interpreted as number of milliseconds
+        //                  since 1 Jan 1970 (a timestamp) 
+        //   a string     : Any format supported by the javascript engine, like
+        //                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+        //  an object     : Interpreted as an object with year, month and date
+        //                  attributes.  **NOTE** month is 0-11.
+        return (
+            d.constructor === Date ? d :
+            d.constructor === Array ? new Date(d[0],d[1],d[2]) :
+            d.constructor === Number ? new Date(d) :
+            d.constructor === String ? new Date(d) :
+            typeof d === "object" ? new Date(d.year,d.month,d.date) :
+            NaN
+        );
+    },
+    compare:function(a,b) {
+        // Compare two dates (could be of any type supported by the convert
+        // function above) and returns:
+        //  -1 : if a < b
+        //   0 : if a = b
+        //   1 : if a > b
+        // NaN : if a or b is an illegal date
+        // NOTE: The code inside isFinite does an assignment (=).
+        return (
+            isFinite(a=this.convert(a).valueOf()) &&
+            isFinite(b=this.convert(b).valueOf()) ?
+            (a>b)-(a<b) :
+            NaN
+        );
+    },
+    inRange:function(d,start,end) {
+        // Checks if date in d is between dates in start and end.
+        // Returns a boolean or NaN:
+        //    true  : if d is between start and end (inclusive)
+        //    false : if d is before start or after end
+        //    NaN   : if one or more of the dates is illegal.
+        // NOTE: The code inside isFinite does an assignment (=).
+       return (
+            isFinite(d=this.convert(d).valueOf()) &&
+            isFinite(start=this.convert(start).valueOf()) &&
+            isFinite(end=this.convert(end).valueOf()) ?
+            start <= d && d <= end :
+            NaN
+        );
+    }
+};
+
+
 function savePTORequest(message) {
 	if (typeof message !== "undefined") {
 		WAF.sources.pTO_Request.emailText = message;
@@ -446,33 +499,16 @@ function signIn() {
 		var currentPTOID = waf.sources.pTO_Request.ID;
 		var noteCollection = WAF.ds.Note.query("pto.ID = " + currentPTOID, {
 			onSuccess: function(event) {
-				event.entityCollection.toArray("title, body", {onSuccess: function(ev) {
+				event.entityCollection.toArray("date, title, body", {onSuccess: function(ev) {
 					var arr = ev.result;
 					var myHTML = '';
 					arr.forEach(function(elem) { 
-						myHTML += '<p class="holiday">' + elem.title + " : " +  elem.body + '</p>';
+						myHTML += '<p class="holiday">' + formatDate(ISOToDate(elem.date)) + " : " + elem.title + " : " +  elem.body + '</p>';
 					});
 					$('#noteContainer').html(myHTML);
 				}});
 			}
 		});
-		
-		
-		/*
-		ds.Note.all({
-			onSuccess:function(event) {
-			event.entityCollection.toArray("title, body", {onSuccess: function(ev) {
-				var arr = ev.result;
-				var myHTML = '';
-				arr.forEach(function(elem) { 
-					myHTML += '<p class="holiday">' + elem.title + " : " +  elem.body + '</p>';
-				});
-				$('#noteContainer').html('Notes:  <br/><br/>' + myHTML);
-			}});
-		}});
-		*/
-		
-		
 	};// @lock
 
 	button1.click = function button1_click (event)// @startlock
@@ -530,7 +566,7 @@ function signIn() {
 			$$('emailBody')._tmpVal = "";
 			$$('emailBody').setValue("");
 			$$('emailMessageDialogTitle').setValue("Enter Message To Be Included in Email to Manager");
-			$('#emailMessageDialog').css("top", 200);
+			$('#emailMessageDialog').css("top", 285);
 			$('#emailMessageDialog').css("left", 400);
 			WAF.widgets['emailMessageDialog'].displayDialog();
 			//WAF.sources.emailMessageObject.body = "";
