8eeb85da0d4c78f86d7fde829a2c811220455e5e
David Robbins <dave@wakanda.org>
2012-08-20 16:23:58 -0700

    added jQuery FullCalendar plugin

---
+++ PTOb201/WebFolder/scripts/calendar.js
@@ -0,0 +1,31 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+		// Init Full Calendar
+		 	$('#calendar').fullCalendar({
+	        // put your options and callbacks here
+	        height: 650
+	        //weekends: false // will hide Saturdays and Sundays
+	        
+	        /*
+	        events: [
+	        	{title  : 'event1', start  : '2012-08-01'},
+	        	{title  : 'event2', start  : '2012-09-05'},
+	        ]
+	        */
+		})
+
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
