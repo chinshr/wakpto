149b6bc9b024202bd396f1d8efeaeeb6c219c20f
David Robbins <dave@wakanda.org>
2012-08-20 16:54:15 -0700

    http request handlers added

--- PTOb201/WebFolder/scripts/calendar.js
+++ PTOb201/WebFolder/scripts/calendar.js
@@ -10,18 +10,18 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
 		// Init Full Calendar
-		 	$('#calendar').fullCalendar({
-	        // put your options and callbacks here
-	        height: 650
-	        //weekends: false // will hide Saturdays and Sundays
-	        
-	        /*
-	        events: [
-	        	{title  : 'event1', start  : '2012-08-01'},
-	        	{title  : 'event2', start  : '2012-09-05'},
-	        ]
-	        */
-		})
+	 	$('#calendar').fullCalendar({
+        // put your options and callbacks here
+        height: 650
+        //weekends: false // will hide Saturdays and Sundays
+        
+        /*
+        events: [
+        	{title  : 'event1', start  : '2012-08-01'},
+        	{title  : 'event2', start  : '2012-09-05'},
+        ]
+        */
+	})
 
 	};// @lock
 
